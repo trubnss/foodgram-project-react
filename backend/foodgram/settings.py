ce69263eee95eb6bcdbb6588504cd088fafbd526
Semen Trubnikov <trubnss@gmail.com>
2023-12-14 14:08:52 +0300

    fix

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -27,6 +27,8 @@ DEBUG = config("DEBUG", default=False, cast=bool)
 ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='',
                        cast=lambda v: [s.strip() for s in v.split(',')])
 
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+
 # Application definition
 
 INSTALLED_APPS = [
