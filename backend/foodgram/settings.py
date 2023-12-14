26c761dcb1d5363b4acf5fba4fcecde13b7d73be
Semen Trubnikov <trubnss@gmail.com>
2023-12-14 14:26:22 +0300

    add CSRF

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -28,6 +28,9 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='',
                        cast=lambda v: [s.strip() for s in v.split(',')])
 
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+CSRF_TRUSTED_ORIGINS = [
+    'https://foodgramfortraining.zapto.org',
+]
 
 # Application definition
 
