099264c0139881dbf09c4a87a64667a7f571f997
Semen Trubnikov <trubnss@gmail.com>
2023-12-14 15:47:23 +0300

    fix

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -129,7 +129,7 @@ STATIC_URL = "/static/"
 STATIC_ROOT = BASE_DIR / "collected_static"
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+MEDIA_ROOT = '/media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
